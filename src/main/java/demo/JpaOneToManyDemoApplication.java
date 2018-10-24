b0a9273c9bfe6edad0a9a6f3a9994097194a43f8
dedaloc2 <joganel@hotmail.com>
2018-10-23 22:02:00 -0500

    Adding new changes

--- src/main/java/demo/JpaOneToManyDemoApplication.java
+++ src/main/java/demo/JpaOneToManyDemoApplication.java
@@ -1,18 +1,25 @@
 package demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableJpaAuditing
-@EnableJpaRepositories
-@EntityScan(basePackages = {"demo.model"})
+//@EnableJpaRepositories
+//@EntityScan(basePackages = {"demo.model"})
 public class JpaOneToManyDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JpaOneToManyDemoApplication.class, args);
-    }
+    	SpringApplication sa  = new SpringApplicationBuilder(JpaOneToManyDemoApplication.class)
+    	.build()
+    	;
+    	sa.setBanner((env,myClass,out) -> out.println("MyBanner"));
+        //SpringApplication.run(JpaOneToManyDemoApplication.class, args);
+    	sa.run(args);
+    	
+    	    }
 }
